33ffa1f475b29b8704d2e5be93b5c06a04e718ce
Jf <ink-v@outlook.com>
2026-03-21 21:10:57 -0500

    add state Terrafform to S3

---
+++ main copy 2.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  region = "us-east-1"
+  
+  assume_role {
+    role_arn = "arn:aws:iam::449112697342:role/tf-deploy-risk-transaction"
+  }
+}
+
+resource "aws_dynamodb_table" "transactions_table" {
+  name           = "BankingTransactions"
+  billing_mode   = "PAY_PER_REQUEST" # Ideal para Free Tier
+  hash_key       = "transaction_id"
+
+  attribute {
+    name = "transaction_id"
+    type = "S"
+  }
+
+  tags = {
+    Environment = "Dev"
+    Project     = "BankingLab"
+  }
+}
+
+resource "aws_iam_role" "lambda_role" {
+  name = "banking_lambda_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess_v2"
+}
